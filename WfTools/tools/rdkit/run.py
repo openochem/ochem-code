361b0a1be1000066790b80d2d882687438182079
bigchem <47360534+bigchem@users.noreply.github.com>
2022-07-21 09:21:31 +0200

    move rdkit descriptors to external tools

--- WfTools/tools/rdkit/run.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    import sys
-    sys.dont_write_bytecode = True
-    import descr
-    from load import read_task
-    task = read_task(sys.argv[1])
-    result = descr.run(task)
